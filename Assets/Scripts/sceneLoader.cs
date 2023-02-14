09ce619641fc263e7399abbf80c485ad2d125ca3
Kağan <kagansensazli@gmail.com>
2023-02-15 02:06:04 +0300

    first commit

--- Assets/Scripts/sceneLoader.cs
+++ Assets/Scripts/sceneLoader.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,8 +8,13 @@ public class sceneLoader : MonoBehaviour
         SceneManager.LoadScene("licensePlate", LoadSceneMode.Single);
     }
 
-    public void setFlagEU()
+    public void setFlagEU_ez()
     {
-        SceneManager.LoadScene("FlagEU", LoadSceneMode.Single);
+        SceneManager.LoadScene("FlagEU_EZ", LoadSceneMode.Single);
+    }
+
+    public void setFlagEU_hd()
+    {
+        SceneManager.LoadScene("FlagEU_HD", LoadSceneMode.Single);
     }
 }
