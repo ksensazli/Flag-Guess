1cb57831d432efdfd9181fd3597e658121df1475
Kağan <kagansensazli@gmail.com>
2023-02-15 15:46:05 +0300

    initial commit

--- Assets/Scripts/mapController.cs
+++ Assets/Scripts/mapController.cs
@@ -7,15 +7,13 @@ public class mapController : MonoBehaviour
 {
     [SerializeField] private List<Button> _cities;
     [SerializeField] private TMPro.TMP_Text _targetCity;
-    [SerializeField] private TMPro.TMP_Text _pointsText;
+    [SerializeField] private TMPro.TMP_Text _clickedText;
     [SerializeField] private TMPro.TMP_Text _plateName;
-    private int _points = 0;
     [SerializeField] private int _targetCityIndex;
     private List<int> _remainingCities = new List<int>();
 
     private void OnEnable()
     {
-        _pointsText.text = _points.ToString();
         for (int i=0; i<_cities.Count; i++)
         {
             int index = i;
@@ -44,21 +42,17 @@ public class mapController : MonoBehaviour
 
     private void onButtonClick(int index)
     {
-        Debug.Log((index + 1) + " " + _cities[index].transform.name + " " + (index == _targetCityIndex));
+        _clickedText.text = (index + 1) + " " + _cities[index].transform.name;
         if(index == _targetCityIndex)
         {
             randomCitySelector();
             _cities[index].image.color = Color.green;
             _cities[index].enabled = false;
-            _points += 10;
-            _pointsText.text = _points.ToString();
         }
         else
         {
             _cities[index].image.color = Color.red;
             _cities[index].enabled = false;
-            _points -= 10;
-            _pointsText.text = _points.ToString();
             DOVirtual.DelayedCall(2f, () => _cities[index].enabled = true);
             DOVirtual.DelayedCall(2f, () => _cities[index].image.color = new Color(0.945098f,0.9333333f,0.8666667f));
         }
